4df1cc4dc4a87ce4f3aac06336c2269c88442528
Yu-Chen Lin <yuchenlin1029@gmail.com>
2025-12-08 23:27:48 -0500

    a simple login api for the login page

---
+++ backend/app/routers/auth.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from datetime import datetime
+
+from app.core.security import hash_password
+from app.core.database import get_db
+from app.models import User, UserCredential, UserProfile
+from app.schemas.user import UserCreate, UserUpdate, UserOut
+
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@router.post("/login")
+def login(payload: LoginRequest, db: Session = Depends(get_db)):
+    # 1. Find by username
+    cred = db.query(UserCredential).filter(
+        UserCredential.username == payload.username
+        ).first()
+    if not cred:
+        raise HTTPException(status_code=400, detail="Username not found")
+    
+    # 2. Compare plaintext passwords (TEMPORARY)
+    if cred.password_hash != payload.password:
+        raise HTTPException(status_code=400, detail="Invalid password")
+
+    # 3. For now, just return the user id (no session yet)
+    return {
+        "message": "Login successful",
+        "user_id": cred.user_id
+    }
